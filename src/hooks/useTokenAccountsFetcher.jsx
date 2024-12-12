b8f71a56cb198746c3b429864521bd76dbb4832b
patrion <patrick.s.davis@gmail.com>
2024-12-12 20:07:16 +0900

    Token Accounts Fetcher as Custom Hook Fixes #34

--- src/hooks/useTokenAccountsFetcher.jsx
+++ src/hooks/useTokenAccountsFetcher.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef } from "react";
 import { PublicKey } from "@solana/web3.js";
 import { useWalletContext } from "@/contexts/WalletContext";
 import { saveUserProfile, removeUserProfile } from "@/utils/localStorageUtils";
-//import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 const TOKEN_MINT_ADDRESS = new PublicKey(
   import.meta.env.VITE_TOKEN_MINT_ADDRESS
@@ -15,32 +14,18 @@ const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey(
   "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
 );
 
-const TokenAccountsFetcher = () => {
+const useTokenAccountsFetcher = () => {
   const { connection, publicKey, setIsTokenHolder } = useWalletContext();
-
   const isFetching = useRef(false);
 
   useEffect(() => {
-    console.log("TokenAccountsFetcher triggered", {
-      publicKey: publicKey?.toBase58(),
-      connection,
-      isFetching: isFetching.current,
-    });
     if (!publicKey || !connection || isFetching.current) {
-      console.log("Skipping fetch. Conditions not met:", {
-        publicKey,
-        connection,
-      });
       return;
     }
 
     const fetchTokenAccount = async () => {
       isFetching.current = true;
       try {
-        console.log(
-          "Querying RPC: findProgramAddress for TOKEN_MINT_ADDRESS: ",
-          TOKEN_MINT_ADDRESS.toBase58()
-        );
         const [ata] = PublicKey.findProgramAddressSync(
           [
             publicKey.toBuffer(),
@@ -49,51 +34,36 @@ const TokenAccountsFetcher = () => {
           ],
           ASSOCIATED_TOKEN_PROGRAM_ID
         );
-        console.log("Looking for ATA:", ata.toBase58());
 
-        // Fetch the account info for the ATA
         const accountInfo = await connection.getAccountInfo(ata);
-        console.log("Account Info:", { connection, accountInfo });
-        // If ATA exists and has a balance, set isTokenHolder to true
         if (accountInfo && accountInfo.data) {
           const balance = accountInfo.data.readUIntLE(64, 8);
-          console.log("Balance:", balance);
           if (balance > 0) {
-            console.log("Tokens Found");
             setIsTokenHolder(true);
-            // Save user profile to localStorage if they are a holder
             saveUserProfile({
               walletAddress: publicKey.toBase58(),
               tokenBalance: balance,
             });
           } else {
-            console.log("Tokens not found.");
             setIsTokenHolder(false);
-            // Remove user profile from localStoage if they are not a holder
             removeUserProfile();
           }
         } else {
-          // If the account does not exist or is empty, set false
-          console.log("Account does not exist, or is empty.");
           setIsTokenHolder(false);
-          // Remove user profile from localStorage if no account is found
           removeUserProfile();
         }
       } catch (error) {
-        console.error("Error feching token accounts:", error);
-        // If an error occurs, clear the user profile
+        console.error("Error fetching token accounts:", error);
         removeUserProfile();
       } finally {
-        isFetching.current = false; // Reset fetch flag.
+        isFetching.current = false;
       }
     };
 
     fetchTokenAccount();
-    // Testing routing
-    setIsTokenHolder(true);
   }, [publicKey, connection, setIsTokenHolder]);
 
   return null;
 };
 
-export default TokenAccountsFetcher;
+export default useTokenAccountsFetcher;

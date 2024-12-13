713ca2a51d75cce33bb8d7d66524d1204fa21ef1
patrion <patrick.s.davis@gmail.com>
2024-12-13 15:58:25 +0900

    Add branding to Footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,19 +1,33 @@
 import { IoLogoTwitter } from "react-icons/io";
+import { AiFillGithub } from "react-icons/ai";
 import "@/styles/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <p className="footer-text">© 2024 My dApp. All rights reserved.</p>
-        <div>
+        <div className="footer-left">
+          <p>© 2024 My dApp. All rights reserved.</p>
+        </div>
+        <div className="footer-center">
+          <a
+            href="https://github.com/PatrionDigital/solanagate"
+            target="_blank"
+            rel="noopener noreferror"
+            className="footer-solanagate"
+          >
+            Built with SolanaGate
+            <AiFillGithub className="footer-icon" />
+          </a>
+        </div>
+        <div className="footer-right">
           <a
             href="https://twitter.com"
             className="footer-link"
             target="_blank"
             rel="noopener noref"
           >
-            <IoLogoTwitter size={20} />
+            <IoLogoTwitter size={20} className="footer-icon" />
           </a>
         </div>
       </div>

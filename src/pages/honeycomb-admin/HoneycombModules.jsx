c072cdbe2695945b6c4cd678df63c93c905b585b
patrion <patrick.s.davis@gmail.com>
2025-03-13 21:51:46 +0900

    Integrate Users and Profiles into Admin Dashboard

--- src/pages/honeycomb-admin/HoneycombModules.jsx
+++ src/pages/honeycomb-admin/HoneycombModules.jsx
@@ -3,6 +3,11 @@ import { HoneycombAdminPage } from "./HoneycombAdminPage";
 import HoneycombDashboard from "./HoneycombDashboard";
 import AdminAuth from "./AdminAuth";
 import * as HoneycombUtils from "./honeycombUtils";
+import AdminUserDashboard from "@/components/user/AdminUserDashboard";
+import ProfileCard from "@/components/profile/ProfileCard";
+import ProfileEditor from "@/components/profile/ProfileEditor";
+import ProfileBadges from "@/components/profile/ProfileBadges";
+import ProfileDashboard from "@/components/profile/ProfileDashboard";
 
 // Export all Honeycomb components
 export {
@@ -10,4 +15,9 @@ export {
   HoneycombDashboard, // Dashboard component
   AdminAuth, // Authentication component
   HoneycombUtils, // Utility functions
+  AdminUserDashboard, // User management dashboard
+  ProfileDashboard, // Profile dashboard
+  ProfileCard, // Profile card component
+  ProfileEditor, // Profile editor component
+  ProfileBadges, // Profile badges component
 };
